03c9ac1034d766eacb6f10f34eb19a08fd0eadaf
Wasim <wasim_miet@yahoo.in>
2018-08-31 19:39:30 +0530

    update

--- BSM Simulator/BSIM.cpp
+++ BSM Simulator/BSIM.cpp
@@ -1,3 +1,9 @@
+//A Static BSM Simulator
+//Compiling BSIM.exe
+//g++ -c -std=c++11 BSIM.cpp
+//g++ -o BSIM.exe BSIM.o -l ws2_32
+//Compiler required on windows : MinGW
+
 #include<iostream>
 #include<cstdio>
 #include<winsock2.h>
@@ -12,7 +18,6 @@ using namespace std;
 
 //#define SERVER "10.10.71.159"
 #define PORT 8888   //The port on which to listen for incoming data
-
 #define history 10
 
 struct BSM{
@@ -30,6 +35,12 @@ double Average_speed;
 long TimeStamp; 
 };
 
+const int periodicity = 100;//in ms
+
+//Simulate the sending BSM of Traffic
+//First randomly generate the number of cars participating
+//Then randomly generate the BSM Id's of these cars
+//Send the data @ 10 Hz
 int main(void)
 {
 	BSM bsm;
@@ -61,9 +72,6 @@ int main(void)
     //si_other.sin_addr.S_un.S_addr = inet_addr(SERVER);
 	si_other.sin_addr.s_addr = INADDR_BROADCAST;//broadcast
 
-	//while(1)
-	
-	//getch();
 	int No_of_BSM;
 	srand(time(NULL));
 	
@@ -94,7 +102,7 @@ int main(void)
 		cout<<*it<<endl;
 	}
 	
-	int counter = 1,size;
+	int counter = 1,size,counterX = 1;
 	
 	std::chrono::high_resolution_clock::time_point t1;
 	std::chrono::high_resolution_clock::time_point t2;
@@ -106,91 +114,93 @@ int main(void)
 	//std::cout << "Started at------------- " << std::ctime(&t_time);
 	//cout<<endl;
 	
-    while(counter <= history)
-    {
-	
-		t2 = std::chrono::high_resolution_clock::now();
-						
-		std::chrono::duration<double, std::milli> time_span = t2 - t1;
-			
-
-	//	cout << "It took me------------------------------- " << time_span.count() << " milliseconds.";
-	//	cout << endl;
+		while(counter <= history)
+		{
 		
-		double wait_time = 0;	
-		double abs_wait_time = 100 - time_span.count();
-		if(abs_wait_time < 0)
-		{
-			wait_time = 0;
-		}
-		else
-		{
-			wait_time = abs_wait_time;
-		}
-		
-	//	cout << "wait time------------------------------- "<< wait_time<<endl;
-		((counter == 1) ? Sleep(1) : Sleep(wait_time));
-			
+			t2 = std::chrono::high_resolution_clock::now();
+							
+			std::chrono::duration<double, std::milli> time_span = t2 - t1;
+				
 
-/*		std::chrono::time_point<std::chrono::system_clock> now = std::chrono::system_clock::now();
-		auto duration = now.time_since_epoch();
-		auto millis = std::chrono::duration_cast<std::chrono::milliseconds>(duration).count();
-		long long int temp;
-		cout<<(millis)<<endl;
-		cout<<"->->->"<<(millis - temp)<<endl;
-		temp = millis;
-		cout<<temp<<endl;
-*/		
-	//	cout<<"Data Set: "<<counter<<endl;
-		int inner_counter = 1;
-		for(it = BSM_Set.begin(); it != BSM_Set.end(); it++)
-		{
-			bsm.Dirty_flag = false;
-			bsm.BSM_Id = *it; 
-			bsm.GPS_Fix = counter; 
-			bsm.Latitude = 18; 
-			bsm.Longitude = 73; 
-			bsm.Altitude = 500;
-			bsm.Lat_Error = 0; 
-			bsm.Long_Error = 0; 
-			bsm.Altitude_Error = 0; 
-			bsm.Speed = *it + 10; 
-			bsm.Average_speed = 10; 
-			bsm.TimeStamp = 12345;//std::chrono::system_clock::to_time_t( now ); 
+		//	cout << "It took me------------------------------- " << time_span.count() << " milliseconds.";
+		//	cout << endl;
 			
-			size = sizeof(bsm);
+			double wait_time = 0;	
+			double abs_wait_time = periodicity - time_span.count();
+			if(abs_wait_time < 0)
+			{
+				wait_time = 0;
+			}
+			else
+			{
+				wait_time = abs_wait_time;
+			}
+			
+		//	cout << "wait time------------------------------- "<< wait_time<<endl;
+			((counterX == 1) ? Sleep(1) : Sleep(wait_time));//maintain the 10 Hz frequency
+			
+			counterX++;
+				
 
-			if (sendto(s, (char*)&bsm, size , 0 , (struct sockaddr *) &si_other, slen) == SOCKET_ERROR)
+	/*		std::chrono::time_point<std::chrono::system_clock> now = std::chrono::system_clock::now();
+			auto duration = now.time_since_epoch();
+			auto millis = std::chrono::duration_cast<std::chrono::milliseconds>(duration).count();
+			long long int temp;
+			cout<<(millis)<<endl;
+			cout<<"->->->"<<(millis - temp)<<endl;
+			temp = millis;
+			cout<<temp<<endl;
+	*/		
+		//	cout<<"Data Set: "<<counter<<endl;
+			int inner_counter = 1;
+			for(it = BSM_Set.begin(); it != BSM_Set.end(); it++)
 			{
-				cout<<"sendto() failed with error code : "<<WSAGetLastError();
-				exit(EXIT_FAILURE);
+				//Static data !
+				bsm.Dirty_flag = false;
+				bsm.BSM_Id = *it; 
+				bsm.GPS_Fix = counter; 
+				bsm.Latitude = 18; 
+				bsm.Longitude = 73; 
+				bsm.Altitude = 500;
+				bsm.Lat_Error = 0; 
+				bsm.Long_Error = 0; 
+				bsm.Altitude_Error = 0; 
+				bsm.Speed = *it + 10; 
+				bsm.Average_speed = 10; 
+				bsm.TimeStamp = 12345;//std::chrono::system_clock::to_time_t( now ); 
+				
+				size = sizeof(bsm);
+
+				if (sendto(s, (char*)&bsm, size , 0 , (struct sockaddr *) &si_other, slen) == SOCKET_ERROR)
+				{
+					cout<<"sendto() failed with error code : "<<WSAGetLastError();
+					exit(EXIT_FAILURE);
+				}
+				if(inner_counter == 1)
+				{
+					t1 = std::chrono::high_resolution_clock::now();
+				}
+				inner_counter++;
+				
+		/*		cout<<"\tBSM Id: "<<bsm.BSM_Id<<endl;
+				cout<<"\tGPS Fix: "<<bsm.GPS_Fix<<endl; 
+				cout<<"\tLatitude: "<<bsm.Latitude<<endl; 
+				cout<<"\tLongitude: "<<bsm.Longitude<<endl; 
+				cout<<"\tAltitude: "<<bsm.Altitude<<endl;
+				cout<<"\tLatitude Error: "<<bsm.Lat_Error<<endl; 
+				cout<<"\tLongitude Error: "<<bsm.Long_Error<<endl; 
+				cout<<"\tAltitude Error: "<<bsm.Altitude_Error<<endl; 
+				cout<<"\tSpeed: "<<bsm.Speed<<endl; 
+				cout<<"\tAverage Speed: "<<bsm.Average_speed<<endl; 
+				cout<<"\tDirty Flag: "<<bsm.Dirty_flag<<endl;
+				cout<<"\tTimeStamp: "<<bsm.TimeStamp<<endl;
+				
+				cout<<endl<<endl;
+		*/
 			}
-			if(inner_counter == 1)
-			{
-				t1 = std::chrono::high_resolution_clock::now();
-			}
-			inner_counter++;
-			
-	/*		cout<<"\tBSM Id: "<<bsm.BSM_Id<<endl;
-			cout<<"\tGPS Fix: "<<bsm.GPS_Fix<<endl; 
-			cout<<"\tLatitude: "<<bsm.Latitude<<endl; 
-			cout<<"\tLongitude: "<<bsm.Longitude<<endl; 
-			cout<<"\tAltitude: "<<bsm.Altitude<<endl;
-			cout<<"\tLatitude Error: "<<bsm.Lat_Error<<endl; 
-			cout<<"\tLongitude Error: "<<bsm.Long_Error<<endl; 
-			cout<<"\tAltitude Error: "<<bsm.Altitude_Error<<endl; 
-			cout<<"\tSpeed: "<<bsm.Speed<<endl; 
-			cout<<"\tAverage Speed: "<<bsm.Average_speed<<endl; 
-			cout<<"\tDirty Flag: "<<bsm.Dirty_flag<<endl;
-			cout<<"\tTimeStamp: "<<bsm.TimeStamp<<endl;
-			
-			cout<<endl<<endl;
-	*/
+			counter++;
 		}
-		counter++;
-    }
 	counter = 1;
-	//cout<<"------------------------------------------------------------------------------------------------------------------"<<endl;
 	}
     closesocket(s);
     WSACleanup();
